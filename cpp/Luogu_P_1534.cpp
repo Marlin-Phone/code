0065e5fef9a0af76814418bd71a359f166aa5787
Marlin <Marlin_Phone@outlook.com>
2025-03-01 23:00:54 +0800

    两道基础题保持洛谷不断

---
+++ cpp/Luogu_P_1534.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+const int N = 3010;
+int n;
+typedef struct {
+    int in_study;
+    int out_study;
+} Study;
+Study stu[N];
+int sum = 0;
+int sum0 = 0;
+
+int main() {
+
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> stu[i].in_study >> stu[i].out_study;
+        sum = sum + stu[i].in_study + stu[i].out_study - 8;
+        sum0 += sum;
+    }
+
+    cout << sum0 << endl;
+
+    return 0;
+}
---
+++ cpp/Luogu_P_5886.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m, p;
+
+int main() {
+    cin >> n >> m >> p;
+    for (int i = 0; i < n; i++) {
+    }
+
+    return 0;
+}
