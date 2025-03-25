44a268c1b6f28a810f4c38abd4897de513cd4834
Marlin <Marlin_Phone@outlook.com>
2025-03-25 23:47:22 +0800

    尝试了一下跳石头,失败.练习了栈,组合型枚举

--- cpp/luogu/text.cpp
+++ cpp/luogu/text.cpp
@@ -1,6 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<> g(0);
+const int N = 20;
+int n, k;
+int arr[N];
+int st[N];
 
-int main() { return 0; }
+void dfs(int x, int start) {
+    if (x > k) {
+        for (int i = 1; i <= k; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        return;
+    }
+    for (int i = start; i <= n - (k - x); i++) {
+        arr[x] = i;
+        st[i] = 1;
+        dfs(x + 1, i + 1);
+        arr[x] = 0;
+        st[i] = 0;
+    }
+}
+int main() {
+    cin >> n >> k;
+    dfs(1, 1);
+
+    return 0;
+}
