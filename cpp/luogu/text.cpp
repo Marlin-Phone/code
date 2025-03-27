8ce827d5d9f0781c58e64e0a82fb0af3187aacfa
Marlin <Marlin_Phone@outlook.com>
2025-03-27 22:39:21 +0800

    做了1道递推, 组合型枚举.

--- cpp/luogu/text.cpp
+++ cpp/luogu/text.cpp
@@ -1,30 +1,32 @@
+// 组合
 #include <bits/stdc++.h>
 using namespace std;
+#define int long long
 
-const int N = 20;
+const int N = 30;
 int n, k;
 int arr[N];
-int st[N];
 
 void dfs(int x, int start) {
     if (x > k) {
         for (int i = 1; i <= k; i++) {
-            cout << arr[i] << " ";
+            cout << arr[i];
         }
         cout << endl;
         return;
     }
-    for (int i = start; i <= n - (k - x); i++) {
+
+    for (int i = start; i <= n; i++) {
+        if (n - i + 1 < k - x + 1) {
+            break;
+        }
         arr[x] = i;
-        st[i] = 1;
         dfs(x + 1, i + 1);
         arr[x] = 0;
-        st[i] = 0;
     }
 }
-int main() {
+
+signed main() {
     cin >> n >> k;
     dfs(1, 1);
-
-    return 0;
 }
---
+++ cpp/蓝桥/116.飞行员兄弟.cpp
@@ -0,0 +1,21 @@
+/*
+ * @acwing app=acwing.cn id=118 lang=C++
+ *
+ * 116. 飞行员兄弟
+ */
+
+// @acwing code start
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+
+const int N = 10;
+char g[N][N];
+
+signed main() {
+    for (int i = 0;)
+
+        return 0;
+}
+
+// @acwing code end
