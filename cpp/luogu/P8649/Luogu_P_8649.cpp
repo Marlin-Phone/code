590f18d8bfa6414535279331ad6a62b565de80ef
Marlin <Marlin_Phone@outlook.com>
2025-04-07 23:09:56 +0800

    练习了前缀和余数的性质,练习了dfs,指数型枚举 笔记: 当区间和sum满足K|sum，即sum mod k=0时，为K的倍数。 而又由于原数组a中al ar的区间和sum等于pr-pl-1（p为a的前缀和数组），所以pr-pl-1≡0(mod k)。 所以pr≡pl-1(mod k)。
    这段数学表述的意思是，如果一个数组的某个子区间的和是 K 的倍数，
    那么这个子区间的右边界对应的前缀和与左边界前一个元素对应的前缀和之差也应该是 K 的倍数。
    进一步地，这意味着右边界的前缀和与左边界前一个元素的前缀和在模 K 的情况下是相等的。(且该子区间的和是 K 的倍数)

---
+++ cpp/luogu/P8649/Luogu_P_8649.cpp
@@ -0,0 +1,51 @@
+// https://luogu.com.cn/problem/P8649
+// 前缀和 前缀和余数的性质 枚举
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define int long long
+
+const int N = 1e5 + 10;
+int n, k;
+int a[N];
+int Sa[N];
+int cnt[N];
+int C[N];
+int first[N];
+int last[N];
+int ans = 0;
+
+// int n1(int x) {
+//     int res = 1;
+//     for (int i = 1; i <= x; i++) {
+//         res *= i;
+//     }
+//     return res;
+// }
+void getC(int x) { // C(n, 2);
+    ans += x * (x - 1) / 2;
+}
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        Sa[i] = (Sa[i - 1] + a[i]) % k;
+    }
+
+    for (int i = 0; i <= n; i++) {
+        cnt[Sa[i]]++;
+    }
+
+    // first[0] = 0;
+    for (int i = 0; i < k; i++) {
+        // ans += cnt[i] * (cnt[i] - 1);
+        getC(cnt[i]);
+    }
+    cout << ans;
+
+    return 0;
+}
