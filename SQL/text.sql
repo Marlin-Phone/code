a011bbe2989bf4cb2f0dc862ec7e37caa4dcd00e
Marlin <Marlin_Phone@outlook.com>
2025-04-14 23:24:20 +0800

    配置好了MySQL的环境,成功编写SQL语句

--- SQL/text.sql
+++ SQL/text.sql
@@ -1,12 +1,32 @@
--- 创建表（分号结束）
-CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) UNIQUE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);  -- ← 注意这里的分号
+CREATE DATABASE egg;
 
--- 新语句开始（无逗号）
-SELECT table_name 
-FROM information_schema.tables 
-WHERE table_schema = DATABASE();
+USE egg;
+CREATE TABLE eggs_record (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    egg_name VARCHAR(10) NOT NULL,
+    sold DATE NULL
+);
+
+INSERT INTO egg.eggs_record (id, egg_name, sold)
+VALUES (1, '鸡蛋', '2021-01-01');
+
+INSERT INTO egg.eggs_record (id, egg_name, sold)
+VALUES (2, '鸭蛋', '2021-02-01');
+
+
+INSERT INTO egg.eggs_record (id, egg_name, sold)
+VALUES (3, '荷包蛋', '2025-04-14');
+
+INSERT INTO egg.eggs_record
+VALUES (DEFAULT, '鹅蛋', '2021-05-01');
+
+DELETE FROM egg.eggs_record
+WHERE id = 19;
+
+UPDATE egg.eggs_record
+SET sold = '999-6-1'
+WHERE id = 20;
+
+SELECT * FROM egg.eggs_record;
+
+SELECT * FROM egg.eggs_record;
