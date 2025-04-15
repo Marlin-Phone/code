fc17bcbdce1e9696e89c44c496e1c41c695dcf85
Marlin <Marlin_Phone@outlook.com>
2025-04-15 23:53:16 +0800

    PTA做了21道题,备赛cccc天梯赛

--- cpp/text.cpp
+++ cpp/text.cpp
@@ -1,11 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int jia, yi;
+int n;
+int jhan, jh, yhan, yh;
+int cnta = 0, cntb = 0;
+
+void check() {
+    if (jh == jhan + yhan && yh != jhan + yhan) {
+        cnta++;
+        jia--;
+    } else if (yh == jhan + yhan && jh != jhan + yhan) {
+        cntb++;
+        yi--;
+    }
+}
 int main() {
-    string s;
-    getline(cin, s);
-    cout << s << endl;
+    cin >> jia >> yi;
+    cin >> n;
+    while (n--) {
+        cin >> jhan >> jh >> yhan >> yh;
+        check();
+        if (jia < 0 || yi < 0) {
+            break;
+        }
+    }
+
+    // cout << "cnta = " << cnta << endl;
+    // cout << "cntb = " << cntb << endl;
+    if (jia < 0) {
+        cout << "A" << endl;
+        cout << cntb;
+    } else {
+        cout << "B" << endl;
+        cout << cnta;
+    }
+
     return 0;
 }
-
-//
