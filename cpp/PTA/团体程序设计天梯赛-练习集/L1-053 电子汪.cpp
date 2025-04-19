37fe6c213f68cb503d667d002c7392d9388c59b4
Marlin <Marlin_Phone@outlook.com>
2025-04-19 22:46:32 +0800

    打了cccc天梯赛,得了134分,但是因为L1得分没有过75分,没有计算L2的得分,最终只有59分(好抽象...)

---
+++ cpp/PTA/团体程序设计天梯赛-练习集/L1-053 电子汪.cpp
@@ -0,0 +1,22 @@
+/*
+  @pintia psid=994805046380707840 pid=994805077443723264 compiler=GXX
+  ProblemSet: 团体程序设计天梯赛-练习集
+  Title: 电子汪
+  https://pintia.cn/problem-sets/994805046380707840/exam/problems/type/7?problemSetProblemId=994805077443723264
+*/
+// @pintia code=start
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b;
+
+int main() {
+    cin >> a >> b;
+    int temp = a + b;
+    for (int i = 1; i <= temp; i++) {
+        cout << "Wang!";
+    }
+
+    return 0;
+}
+// @pintia code=end
