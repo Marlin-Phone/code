1e6a1e4bd76be5fc4e8f51f02751ac25e1e3b669
Marlin <Marlin_Phone@outlook.com>
2025-04-26 23:50:19 +0800

    系统学习sql单表语法

--- SQL/text.sql
+++ SQL/text.sql
@@ -1,16 +1,50 @@
-CREATE TABLE nowcoder_tbl(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nowcoder_title VARCHAR(255),
-    nowcoder_author VARCHAR(255),
-    submission_date DATE
+
+CREATE Table `employee`(
+    `emp_id` INT PRIMARY KEY,
+    `name` VARCHAR(20),
+    `birth_date` DATE,
+    `sex` VARCHAR(1),
+    `salary` INT,
+    `branch_id` INT,
+    `sup_id` INT
+);
+
+CREATE Table `branch`(
+    `branch_id` INT PRIMARY KEY,
+    `branch_name` VARCHAR(20),
+    `manager_id` INT,
+    Foreign Key (`manager_id`) REFERENCES `employee`(`emp_id`) ON DELETE SET NULL
 )
 
-SELECT * FROM nowcoder_tbl WHERE nowcoder_author = "marlin_phone" OR id = 1;
 
-update nowcoder_tbl 
-set nowcoder_title = '学习 PHP'
-where id = 1;
+ALTER Table `employee`
+ADD FOREIGN KEY (`branch_id`) REFERENCES `branch`(`branch_id`)
+ON DELETE SET NULL;
 
-select * 
-FROM nowcoder_tbl
-WHERE nowcoder_title LIKE ''
+ALTER Table `employee`
+ADD FOREIGN KEY (`sup_id`)
+REFERENCES `employee`(`emp_id`)
+ON DELETE SET NULL;
+
+CREATE Table `client`(
+    `client_id` INT PRIMARY KEY,
+    `client_name` VARCHAR(20),
+    `phone` varchar(20)
+)
+
+create table `work_with`(
+    `emp_id` INT,
+    `client_id` INT,
+    `total_sales` INT,
+    PRIMARY KEY (`emp_id`, `client_id`),
+    Foreign Key (`emp_id`) REFERENCES `employee`(`emp_id`) ON DELETE CASCADE,
+    Foreign Key (`client_id`) REFERENCES `client`(`client_id`) ON DELETE CASCADE
+)
+
+INSERT INTO `employee`
+
+select *
+FROM employee;
+
+select *
+FROM branch;
