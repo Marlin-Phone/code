eebbfdfd09dd3a91fe144496a8b51a2adea8eaf4
Marlin <Marlin_Phone@outlook.com>
2025-05-16 18:19:52 +0800

    二叉树的层序序列化和反序列化

--- test.cpp
+++ test.cpp
@@ -1,33 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string str;
-vector<string> ans;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int val) : val(val) {};
+};
+queue<>
 
-int main() {
-    getline(cin, str);
-    if (str.empty()) {
-        return 0;
-    }
-
-    size_t start = 0;
-
-    for (size_t i = 0; i < str.size();) {
-        if (str[i] == ',' || str[i] == ' ') {
-            if (i > start) {
-                ans.push_back(str.substr(start, i - start));
-            }
-            start = i + 1;
-        }
-        i++;
-    }
-    if (start < str.size()) {
-        ans.push_back(str.substr(start));
-    }
-
-    for (int i = 0; i < ans.size(); i++) {
-        cout << ans[i] << endl;
-    }
+    int main() {
 
     return 0;
 }
