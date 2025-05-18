c60ec7fc95d787a93bc626581ad844ee1c8fae64
Marlin <Marlin_Phone@outlook.com>
2025-05-18 21:45:18 +0800

    牛客打了一场比赛

---
+++ cpp/牛客/牛客周赛 Round 93/死.cpp
@@ -0,0 +1,27 @@
+// https://luogu.com.cn/problem/P
+//
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+// #define int long long
+
+const int N = 2e5 + 10;
+int T = 1;
+int n, k;
+int a[N];
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> T;
+    while (T--) {
+        cin >> n >> k;
+        for (int i = 0; i < n; i++){
+            cin >> a[i];
+        }
+
+    }
+    return 0;
+}
