96ea1243252bf862558ddf92418731d4d1da9d55
Marlin <Marlin_Phone@outlook.com>
2025-06-23 23:55:27 +0800

    [算法]:正在学习拓扑排序,还没A题

--- Luogu_P_1113.cpp
+++ Luogu_P_1113.cpp
@@ -6,14 +6,48 @@ using namespace std;
 #define debug(a) cout << #a << " = " << endl;
 // #define int long long
 
-// const int N =
+const int N = 1e4 + 10;
 int T = 1;
 int n;
+// struct node{
+//     vector<int> neighbor;
+//     int val;
+// };
+vector<int> inDegree(N);
+vector<int> g[N];
+queue<int> q;
 
 void solve() {
     cin >> n;
-    for (int i = 0; i < n; i++) {
+    for (int i = 1; i <= n; i++) {
+        int uu;
+        int val;
+        while (1) {
+            int vv;
+            cin >> vv;
+            if (vv == 0) {
+                break;
+            }
+            g[vv].push_back(uu);
+            inDegree[uu]++;
+        }
     }
+
+    for (int i = 1; i <= n; i++) {
+        if (inDegree[i] == 0) {
+            q.push(i);
+        }
+    }
+
+    while (!q.empty()) {
+    }
+
+    // for (int i = 1; i <= n; i++) {
+    //     for (int j = 0; j < vec[i].size(); j++) {
+    //         cout << vec[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
 }
 
 signed main() {
---
+++ Luogu_P_12832.cpp
@@ -0,0 +1,31 @@
+// https://luogu.com.cn/problem/P12832
+//
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define debug(a) cout << #a << " = " << endl;
+// #define int long long
+
+// const int N =
+int T = 1;
+int n, k;
+
+void solve() {
+    cin >> n >> k;
+    if (n % 2 == 0) {
+        if (k % 2)
+    } else {
+    }
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
