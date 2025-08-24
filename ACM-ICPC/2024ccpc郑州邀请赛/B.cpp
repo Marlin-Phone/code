c3e1baa92b9508a9ada555bedea1115ad9270c66
Marlin <Marlin_Phone@outlook.com>
2025-08-24 21:48:48 +0800

    [算法]:vp了2024ccpc郑州邀请赛

---
+++ ACM-ICPC/2024ccpc郑州邀请赛/B.cpp
@@ -0,0 +1,75 @@
+#pragma GCC optimize("O3", "inline", "omit-frame-pointer", "unroll-loops",     \
+                     "fast-math")
+#include <bits/stdc++.h>
+using namespace std;
+
+// Types
+#define ll long long
+#define int ll
+#define float double
+// Aliases
+#define x first
+#define y second
+#define endl '\n'
+// Comparison
+#define is0(x) (fabs(x) < eps)
+#define feq(x, y) (is0(x - y))
+#define fge(x, y) (x > y || feq(x, y))
+#define fle(x, y) (x < y || feq(x, y))
+#define peq(a, b) (is0(a.x - b.x) && is0(a.y - b.y))
+// Math
+#define mod(x) ((x + MOD) % MOD)
+#define updiv(u, d) ((u + d - 1) / d)
+// Debug
+#define dbg(a) cout << "Dbg: " << #a << " = " << a << endl;
+
+// const db eps = 1e-8;
+// const int MOD = 1e9 + 7;
+const int N = 1e6 + 10;
+
+int n;
+struct node {
+    int c, idx;
+} node[N];
+
+void solve() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> node[i].c;
+        node[i].idx = i;
+    }
+    sort(node + 1, node + 1 + n,
+         [](struct node &a, struct node &b) { return a.c < b.c; });
+    // for (int i = 1; i <= n; i++) {
+    //     cout << node[i].c << " " << node[i].idx << endl;
+    // }
+    int cos = 0;
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+        if (node[i].idx <= cos) {
+            continue;
+        }
+        int temp = (node[i].idx - cos) / node[i].c;
+        ans += temp;
+        cos += node[i].c * temp;
+        // dbg(i);
+        // dbg(temp);
+        // dbg(cos);
+        // dbg(ans);
+    }
+    cout << ans << endl;
+}
+
+signed main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
