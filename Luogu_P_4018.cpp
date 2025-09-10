b40138c0c4eae7f6f23140c5280df3c350345f1c
Marlin <Marlin_Phone@outlook.com>
2025-09-10 23:48:46 +0800

    [算法]: 学习了巴什博弈扩展

---
+++ Luogu_P_4018.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3", "inline", "omit-frame-pointer", "unroll-loops",     \
+//                      "fast-math")
+using namespace std;
+
+// Types
+#define ll long long
+#define int ll
+#define float double
+// Aliases
+#define x first
+#define y second
+#define endl '\n'
+// Math
+#define mod(x) ((x + MOD) % MOD)
+// Debug
+#define dbg(a) cout << "Dbg: " << #a << " = " << a << endl;
+
+// const db eps = 1e-8;
+// const int MOD = 1e9 + 7;
+// const int N = 1e6 + 10;
+
+int n;
+
+void solve() {
+    cin >> n;
+    if (n % 6 != 0) {
+        cout << "October wins!" << endl;
+    } else {
+        cout << "Roy wins!" << endl;
+    }
+}
+
+signed main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
