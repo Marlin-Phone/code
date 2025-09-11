f5f5e8469b38408df3aaaa1d44c94b4960362ce0
Marlin <Marlin_Phone@outlook.com>
2025-09-11 23:48:50 +0800

    [算法]: 练习了尼姆博弈

---
+++ cpp/左程云算法/P2197/Luogu_P_2197.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+// #pragma GCC optimize("O3", "inline", "omit-frame-pointer", "unroll-loops",     \
+//                      "fast-math")
+using namespace std;
+
+// Types
+#define ll long long
+#define int ll
+#define float double
+// Aliases
+#define x first
+#define y second
+#define endl '\n'
+// Math
+#define mod(x) ((x + MOD) % MOD)
+// Debug
+#define dbg(a) cout << "Dbg: " << #a << " = " << a << endl;
+
+// const db eps = 1e-8;
+// const int MOD = 1e9 + 7;
+// const int N = 1e6 + 10;
+
+void solve() {
+    int n;
+    cin >> n;
+    int S = 0;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        S ^= num;
+    }
+    if (S == 0) {
+        cout << "No" << endl;
+    } else {
+        cout << "Yes" << endl;
+    }
+}
+
+signed main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
---
+++ cpp/左程云算法/尼姆博弈.cpp
@@ -0,0 +1,58 @@
+#pragma GCC optimize("O3", "inline", "omit-frame-pointer", "unroll-loops",     \
+                     "fast-math")
+#include <bits/stdc++.h>
+using namespace std;
+
+// Types
+#define ll long long
+#define int ll
+#define float double
+// Aliases
+#define x first
+#define y second
+#define endl '\n'
+// Comparison
+#define is0(x) (fabs(x) < eps)
+#define feq(x, y) (is0(x - y))
+#define fge(x, y) (x > y || feq(x, y))
+#define fle(x, y) (x < y || feq(x, y))
+#define peq(a, b) (is0(a.x - b.x) && is0(a.y - b.y))
+// Math
+#define mod(x) ((x + MOD) % MOD)
+#define updiv(u, d) ((u + d - 1) / d)
+// Debug
+#define dbg(a) cout << "Dbg: " << #a << " = " << a << endl;
+
+// const db eps = 1e-8;
+// const int MOD = 1e9 + 7;
+const int N = 1e6 + 10;
+
+int n;
+int S = 0;
+int arr[N];
+
+void solve() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        cout << bitset<6>(arr[i]) << endl;
+        S ^= arr[i];
+    }
+    cout << endl;
+    cout << bitset<6>(S) << endl;
+    cout << S << endl;
+}
+
+signed main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios::sync_with_stdio(false);
+
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
